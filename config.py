dc5732cdc802d85ff827f7f17694a59df961ccea
lamyae-sr <lamyae.sersar@usmba.ac.ma>
2026-08-06 00:20:58 +0100

    Correction config

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
-HOST = os.environ.get("MY_SQLhost")
-USER = os.environ.get("user")
-PASSWORD = os.environ.get("password")
-DATABASE = os.environ.get("database")
-PORT = int(os.environ.get("MY_SQLPORT"))
+HOST = os.environ.get("MY_SQLhost", "")
+USER = os.environ.get("user", "")
+PASSWORD = os.environ.get("password", "")
+DATABASE = os.environ.get("database", "")
+
+PORT_STR = os.environ.get("MY_SQLPORT", "3306")
+PORT = int(PORT_STR)
