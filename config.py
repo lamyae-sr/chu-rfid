3975c20b27155032f19f12f017e4ba2aa9139502
lamyae-sr <lamyae.sersar@usmba.ac.ma>
2026-08-05 23:37:56 +0100

    Correction config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-HOST = os.environ.get("HOST")
+HOST = os.environ.get("MY_SQLhost")
 USER = os.environ.get("USER")
 PASSWORD = os.environ.get("PASSWORD")
 DATABASE = os.environ.get("DATABASE")
-PORT = int(os.environ.get("PORT"))
+PORT = int(os.environ.get("MY_SQLport"))
